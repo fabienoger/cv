04c2e83a3ff755cc3f1bb383d86db203f0cf1e23
fabienoger <fab-oger@live.fr>
2015-09-21 15:08:58 +0200

    add helpers age

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,13 @@
 module PagesHelper
+  def age
+    t = Time.new
+    day = 9;
+    month = 7
+    year = 1996
+    age = t.year - year
+    if t.month - month < 0 || t.day - day < 0
+      age = age - 1
+    end
+    age
+  end
 end
