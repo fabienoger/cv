f8a85f8f74a5fb8a16da668c6f333660ae8e8058
fabienoger <fab-oger@live.fr>
2015-10-08 12:42:36 +0200

    change solde.int to solde.flaot

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150929124448) do
+ActiveRecord::Schema.define(version: 20151008103502) do
 
   create_table "creations", force: :cascade do |t|
     t.string   "title"
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 20150929124448) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.integer  "solde"
+    t.float    "solde"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
