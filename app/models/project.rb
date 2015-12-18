7effd0484b607dc830ebc2c9320089ebab798f9c
fabienoger <fab-oger@live.fr>
2015-12-18 10:28:46 +0100

    Change image size & work in project/:id

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ActiveRecord::Base
-  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :picture, styles: { large: "800x400>", medium: "300x150>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 end
